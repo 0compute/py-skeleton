9604ac6492525b979973014f9f2e24c9a0101d8f
Arthur Noel <arthur@ischium.net>
2024-12-10 06:27:49 +0000

    covcfg remove zerolib dependency

--- covcfg.py
+++ covcfg.py
@@ -6,9 +6,7 @@ from pathlib import Path
 
 import deepmerge
 import toml
-from zerolib import Dic
 
-# FIXME: the project has to have installed the dependencies of this script
 if __name__ == "__main__":
     # read coverage config from pyproject.toml
     # merge covcfg.toml on to it if it exists
@@ -17,7 +15,7 @@ if __name__ == "__main__":
     # 1st arg is pyproject.toml, 2nd is either a path to covcfg or a name
     pyproject, covcfg = map(Path, sys.argv[1:3])
 
-    coverage = Dic(toml.load(pyproject)).tool.coverage
+    coverage = toml.load(pyproject)["tool"]["coverage"]
 
     if covcfg.exists():
         deepmerge.always_merger.merge(coverage, toml.load(covcfg))
@@ -25,17 +23,17 @@ if __name__ == "__main__":
     else:
         name = covcfg.name
 
-    coverage.run.data_file += f"-{name}"
+    coverage["run"]["data_file"] += f"-{name}"
 
-    coverage.html.directory += f"-{name}"
-    coverage.html.title = f"{name.capitalize()} Test Coverage Report"
+    coverage["html"]["directory"] += f"-{name}"
+    coverage["html"]["title"] = f"{name.capitalize()} Test Coverage Report"
 
-    xml = Path(coverage.xml.output)
-    coverage.xml.output = f"{xml.parent}/{xml.stem}-{name}{xml.suffix}"
+    xml = Path(coverage["xml"]["output"])
+    coverage["xml"]["output"] = f"{xml.parent}/{xml.stem}-{name}{xml.suffix}"
 
     for conf in coverage.values():
         for value in conf.values():
             if isinstance(value, list):
                 value.sort()
 
-    toml.dump(dict(tool=dict(coverage=coverage)), sys.stdout)  # noqa: C408
+    toml.dump(dict(tool=dict(coverage=coverage)), sys.stdout)
